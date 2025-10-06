8c6bdb2c527771e6fa4462ad9ddac629bbbaf7c8
thewarbringer <aadityanarayansharma771@gmail.com>
2025-10-06 15:58:14 +0530

    pausing

--- FiksIt/Main/urls.py
+++ FiksIt/Main/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.Landing,name='Landing'),
+    path('new', views.File_new,name='New_complain'),
+    path('register', views.Sign_Up,name='New_User'),
     
 ]
--- FiksIt/Main/views.py
+++ FiksIt/Main/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def Landing(request):
     return render(request,'Main/index.html')
+def File_new(request):
+    return render(request,'Main/file_complain.html')
+def Sign_Up(request):
+    return render(request,'Main/signup.html')

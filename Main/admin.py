83b89f0d30581f24a549b323aefdef5c0ff07c18
thewarbringer <aadityanarayansharma771@gmail.com>
2025-10-10 01:31:29 +0530

    database model changes applied

--- Main/admin.py
+++ Main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User,Worker,Complain
+
+admin.site.register(User)
+admin.site.register(Worker)
+admin.site.register(Complain)
